048976d30bed235502591a2587e99cce15b468ff
devson <sonyc5720@gmail.com>
2021-07-25 20:53:42 +0900

    [#12] feature: add Domain Member Module `SpringApplication` startup code

--- src/domain/member/src/main/kotlin/com/tistory/devs0n/bingewatching/domain/member/DomainMemberModule.kt
+++ src/domain/member/src/main/kotlin/com/tistory/devs0n/bingewatching/domain/member/DomainMemberModule.kt
@@ -1,6 +1,12 @@
 package com.tistory.devs0n.bingewatching.domain.member
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Import
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackageClasses = [DomainMemberModule::class])
 class DomainMemberModule
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@Import(DomainMemberModule::class)
+annotation class EnableDomainMemberModule
