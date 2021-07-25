63d3b3f57678d559bff5085715a7501e497fe03a
devson <sonyc5720@gmail.com>
2021-07-25 18:13:31 +0900

    [#12] feature: add `findById` method

--- src/domain/member/src/main/kotlin/com/tistory/devs0n/bingewatching/domain/member/core/domain/MemberRepository.kt
+++ src/domain/member/src/main/kotlin/com/tistory/devs0n/bingewatching/domain/member/core/domain/MemberRepository.kt
@@ -1,11 +1,19 @@
 package com.tistory.devs0n.bingewatching.domain.member.core.domain
 
+import java.util.*
+
 interface MemberRepository {
     /**
      * @throws com.tistory.devs0n.bingewatching.domain.member.core.domain.exceptions.MemberRepositoryException
      * if fail to integrate with MemberRepository infrastructure
      */
     fun save(member: Member): Member
+
+    /**
+     * @throws com.tistory.devs0n.bingewatching.domain.member.core.domain.exceptions.MemberRepositoryException
+     * if fail to integrate with MemberRepository infrastructure
+     */
+    fun findById(id: UUID): Member?
 
     /**
      * @throws com.tistory.devs0n.bingewatching.domain.member.core.domain.exceptions.MemberRepositoryException
