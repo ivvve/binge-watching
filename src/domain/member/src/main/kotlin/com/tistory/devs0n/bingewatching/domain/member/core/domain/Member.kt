0b6a347d7db902a19b2a430713a1d329af8bc32f
devson <sonyc5720@gmail.com>
2021-07-25 17:39:12 +0900

    [#12] feature: add `Member` domain entity

---
+++ src/domain/member/src/main/kotlin/com/tistory/devs0n/bingewatching/domain/member/core/domain/Member.kt
@@ -0,0 +1,38 @@
+package com.tistory.devs0n.bingewatching.domain.member.core.domain
+
+import com.tistory.devs0n.bingewatching.domain.member.common.entity.UUIDEntity
+import com.tistory.devs0n.bingewatching.domain.member.common.exceptions.InvalidDomainValueException
+import com.tistory.devs0n.bingewatching.domain.member.common.utils.hasLengthBetween
+import com.tistory.devs0n.bingewatching.domain.member.common.utils.hasSpace
+import org.springframework.data.relational.core.mapping.Column
+import org.springframework.data.relational.core.mapping.Table
+
+@Table("members")
+class Member : UUIDEntity {
+    @Column("username")
+    var username: String
+
+    private constructor(username: String) : super() {
+        this.validateUsername(username)
+        this.username = username
+    }
+
+    private fun validateUsername(username: String) {
+        if (username.hasSpace() ||
+            username.isBlank() ||
+            username.hasLengthBetween(USERNAME_MIN_LENGTH, USERNAME_MAX_LENGTH)
+        ) {
+            throw InvalidDomainValueException("Member.username")
+        }
+    }
+
+    companion object {
+        const val USERNAME_MAX_LENGTH = 20
+        const val USERNAME_MIN_LENGTH = 4
+
+        fun new(username: String): Member {
+            return Member(username)
+                .apply { this.new = true }
+        }
+    }
+}
