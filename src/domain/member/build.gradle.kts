f5966625e59133656ac1af6e5b7d0ebe00e11849
devson <sonyc5720@gmail.com>
2021-07-25 11:49:37 +0900

    [#12] build: add `com.fasterxml.uuid:java-uuid-generator` to generate sequential UUID

--- src/domain/member/build.gradle.kts
+++ src/domain/member/build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
 }
 
 dependencies {
+    implementation("com.fasterxml.uuid:java-uuid-generator:4.0")
+
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
     runtimeOnly("mysql:mysql-connector-java")
 }
