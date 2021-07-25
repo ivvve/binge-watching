b0ba09ca739d6ea90d1ca9b3094ccbd3332d612b
devson <sonyc5720@gmail.com>
2021-07-25 20:06:07 +0900

    [#12] test: add `DataJdbcMemberRepositoryTest` data jdbc test

---
+++ src/domain/member/src/test/kotlin/com/tistory/devs0n/bingewatching/domain/member/core/infrastructure/DataJdbcMemberRepositoryTest.kt
@@ -0,0 +1,44 @@
+package com.tistory.devs0n.bingewatching.domain.member.core.infrastructure
+
+import com.tistory.devs0n.bingewatching.domain.member.core.domain.Member
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+
+@DisplayName("JdbcMemberRepository")
+internal class DataJdbcMemberRepositoryTest : DataJdbcMemberTestInitializer() {
+    @Autowired
+    lateinit var memberRepository: DataJdbcMemberRepository
+
+    @Test
+    fun `save - Member를 저장한다`() {
+        // given
+        val member = Member.new("chris")
+
+        // when
+        memberRepository.save(member)
+
+        // then
+        val savedMember = memberRepository.findById(member.id)!!
+        assertThat(savedMember).isEqualTo(member)
+    }
+
+    @Test
+    fun `existsByUsername - 해당 username의 Member 저장 유무를 리턴한다`() {
+        // given
+        val username = "chris"
+
+        // when
+        val shouldBeFalse = memberRepository.existsByUsername(username)
+        // then
+        assertThat(shouldBeFalse).isFalse
+
+        // given
+        memberRepository.save(Member.new(username))
+        // when
+        val shouldBeTrue = memberRepository.existsByUsername(username)
+        // then
+        assertThat(shouldBeTrue).isTrue
+    }
+}
