73a7ceda6479a2e1c8e611b1be911f589d7eebed
devson <sonyc5720@gmail.com>
2021-07-27 22:03:35 +0900

    [#14] test: change test skipping annotation

--- src/application/api/src/test/kotlin/com/tistory/devs0n/application/api/ApiApplicationTests.kt
+++ src/application/api/src/test/kotlin/com/tistory/devs0n/application/api/ApiApplicationTests.kt
@@ -1,14 +1,14 @@
 package com.tistory.devs0n.application.api
 
-import org.junit.Ignore
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
+@Disabled
 @SpringBootTest
 class ApiApplicationTests {
 
     @Suppress("EmptyFunctionBlock")
-    @Ignore
     @Test
     fun contextLoads() {
         // TODO set up integration test environment
