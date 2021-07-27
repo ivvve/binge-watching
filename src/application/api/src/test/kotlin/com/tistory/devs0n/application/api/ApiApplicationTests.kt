f63b1ad59d623e5b2f84f1d02b9f7ae6645c3f13
devson <sonyc5720@gmail.com>
2021-07-27 21:56:31 +0900

    [#14] misc: add comment for application test

--- src/application/api/src/test/kotlin/com/tistory/devs0n/application/api/ApiApplicationTests.kt
+++ src/application/api/src/test/kotlin/com/tistory/devs0n/application/api/ApiApplicationTests.kt
@@ -1,5 +1,6 @@
 package com.tistory.devs0n.application.api
 
+import org.junit.Ignore
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
@@ -7,8 +8,10 @@ import org.springframework.boot.test.context.SpringBootTest
 class ApiApplicationTests {
 
     @Suppress("EmptyFunctionBlock")
+    @Ignore
     @Test
     fun contextLoads() {
+        // TODO set up integration test environment
     }
 
 }
